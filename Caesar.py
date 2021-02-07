c6624eada062b6649e1019dd554b5af843c5f906
Rafael Contín <35207124+rafaelcontin@users.noreply.github.com>
2021-02-07 04:44:32 -0400

    Add files via upload

---
+++ Caesar.py
@@ -0,0 +1,36 @@
+"""
+Práctica 1 - Fundamentos de Criptografía
+Script de Cifrado de Sustitución César
+Cipher Caesar
+@author: Rafael Contín
+"""
+
+import string
+
+texto_original = input("Digite la palabra o frase que quiere cifrar: ").lower()
+
+while True:
+    try:
+        desplazamiento = int(input("Digite el número de posiciones a desplazar la palabra o frase: "))
+        break
+    except ValueError:
+        print("Digite solo números enteros")
+
+alfabeto = list(string.ascii_lowercase)
+
+def cifradoCesar(original,desplazo,alfabeto):
+    texto_cifrado = ''
+    for i in original:
+        while True:
+            try:
+                posicion = (alfabeto.index(i) + desplazo) % 26
+                texto_cifrado += alfabeto[posicion]
+                break
+            except ValueError:
+                texto_cifrado += i
+                break
+
+    return texto_cifrado
+
+texto_cifrado = cifradoCesar(texto_original,desplazamiento,alfabeto)
+print('La palabra o frase cifrada es:',texto_cifrado)
